71de53c9c43b8bff6190bc7420c096177df629bd
Gian Anthony Choque <140866954+Anthonymss@users.noreply.github.com>
2025-07-20 20:55:06 -0500

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ const API_URL_LOCAL: string = 'http://localhost:3000/api';
 const API_URL_PRODUCTION: string = 'https://yachaywasiback.shop/api';
 
 const api: AxiosInstance = axios.create({
-  baseURL: API_URL_LOCAL, 
+  baseURL: API_URL_PRODUCTION, 
   headers: {
     "Content-Type": "application/json",
   },
